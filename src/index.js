eb9493e580b652bdd036d3c7c0593308834538e3
ezrankayamba <ezrankayamba@gmail.com>
2020-04-07 10:24:37 +0300

    Added Snackbar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as CommonForm } from "./forms/CommonForm";
 export { default as Modal } from "./modal/Modal";
 export { default as CloseableModal } from "./modal/CloseableModal";
+export { default as Snackbar } from "./notify/Snackbar";
